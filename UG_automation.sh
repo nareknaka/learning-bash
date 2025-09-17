970beec84e85409a2bdd886de8ead89c80d53882
Narek <narek.grigoryan1993@gmail.com>
2025-09-17 14:28:15 +0400

    add command to crieate devops group

--- UG_automation.sh
+++ UG_automation.sh
@@ -5,3 +5,4 @@
 # Add three new users (dev1, dev2, dev3) to that group.
 # Each should have a home directory, restricted permissions, and their own .bashrc with a custom welcome message.
 
+sudo groupadd devops
