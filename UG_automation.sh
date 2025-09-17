c997c187d5b892814a769bbfbf57b76b30b0074a
Narek <narek.grigoryan1993@gmail.com>
2025-09-17 13:13:30 +0400

    Add automation script to create devops group and users

---
+++ UG_automation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# User and Group Automation Script
+# Write a script that creates a group devops.
+# Add three new users (dev1, dev2, dev3) to that group.
+# Each should have a home directory, restricted permissions, and their own .bashrc with a custom welcome message.
+
