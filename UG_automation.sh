3f4e54287d04f272bc53d9407d40b6bdc70906b7
Narek <narek.grigoryan1993@gmail.com>
2025-09-18 13:26:40 +0400

    Update UG_automation.sh with user and group automation script

--- UG_automation.sh
+++ UG_automation.sh
@@ -6,3 +6,9 @@
 # Each should have a home directory, restricted permissions, and their own .bashrc with a custom welcome message.
 
 sudo groupadd devops
+for user in dev1 dev2 dev3; do
+    sudo useradd -m -g devops -s /bin/bash "$user"
+    echo "Welcome to the system, $user!" | sudo tee /home/"$user"/.bashrc
+    sudo chmod 700 /home/"$user"
+done
+
