2dff2e59e423d59b80133c9837f10e38c3581746
Narek <narek.grigoryan1993@gmail.com>
2025-09-24 10:29:28 +0400

    add process identification step

--- process_monitorring.sh
+++ process_monitorring.sh
@@ -5,7 +5,7 @@
 # If usage exceeds a threshold (say 70%), log the event with timestamp into /var/log/custom_monitor.log.
 
 PROCESS_NAME="nginx"    
-CPU_THRESHOLD=70
+CPU_THRESHOLD=70 
 MEM_THRESHOLD=70
 LOG_FILE="/var/log/process_monitor.log"
 sudo touch $LOG_FILE
@@ -16,3 +16,6 @@ if ! pgrep "$PROCESS_NAME" > /dev/null; then #/dev/null to not show output
     echo "$(date): Process $PROCESS_NAME not found" >> "$LOG_FILE"
     exit 1
 fi
+
+# Get PID
+PID=$(pgrep "$PROCESS_NAME" | head -1)
