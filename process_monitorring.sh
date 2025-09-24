0466fd0816c147381c01321716092d04c8761ecc
Narek <narek.grigoryan1993@gmail.com>
2025-09-24 09:23:48 +0400

    add process monitoring script template

---
+++ process_monitorring.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Process Monitoring Script
+# Write a bash script that monitors CPU and memory usage of a specific process (e.g., nginx or sshd or anything else).
+# If usage exceeds a threshold (say 70%), log the event with timestamp into /var/log/custom_monitor.log.
+
