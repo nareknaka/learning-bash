bc02d38fb17d3f6d9e458cf1513592db43e8cb31
Narek <narek.grigoryan1993@gmail.com>
2025-09-26 12:38:21 +0400

    add home usage warning when exceeding 80%

--- disk_usage_monitor.sh
+++ disk_usage_monitor.sh
@@ -4,3 +4,7 @@
 # If usage exceeds 80%, send a warning message to all logged-in users using wall.
 
 home_usage=$(df /home | grep /home | awk '{print $5}' | cut -d'%' -f1)
+
+if [ $home_usage -gt 80 ]; then
+    echo "WARNING: /home usage is ${home_usage}%" | wall
+fi
