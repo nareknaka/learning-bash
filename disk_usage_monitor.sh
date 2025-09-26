2e4d152a1e6ff0309f0efa2985c5cd7d1b1add89
Narek <narek.grigoryan1993@gmail.com>
2025-09-26 12:31:03 +0400

    fix syntax error: in line 6

--- disk_usage_monitor.sh
+++ disk_usage_monitor.sh
@@ -3,5 +3,4 @@
 # Script to check free space on /home and /var.
 # If usage exceeds 80%, send a warning message to all logged-in users using wall.
 
-home_usage+$(df /home | grep /home | awk '{print $5}' | cut -d'%' -f1)
-
+home_usage=$(df /home | grep /home | awk '{print $5}' | cut -d'%' -f1)
