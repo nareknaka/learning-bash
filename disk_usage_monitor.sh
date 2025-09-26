a77e534fc02c4ba72b59ede164967eb068a564db
Narek <narek.grigoryan1993@gmail.com>
2025-09-26 12:27:00 +0400

    add initial disk usage monitor script

--- disk_usage_monitor.sh
+++ disk_usage_monitor.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 # Disk Usage Monitor
 # Script to check free space on /home and /var.
-# If usage exceeds 80%, send a warning message to all logged-in users using 
+# If usage exceeds 80%, send a warning message to all logged-in users using wall.
+
+home_usage+$(df /home | grep /home | awk '{print $5}' | cut -d'%' -f1)
+
