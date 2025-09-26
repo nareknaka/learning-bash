ac5d4a8e0e7031c2138133f020e170e1b8f9c7ac
Narek <narek.grigoryan1993@gmail.com>
2025-09-26 12:51:42 +0400

    fix disk usage error

--- disk_usage_monitor.sh
+++ disk_usage_monitor.sh
@@ -3,8 +3,8 @@
 # Script to check free space on /home and /var.
 # If usage exceeds 80%, send a warning message to all logged-in users using wall.
 
-home_usage=$(df /home | grep /home | awk '{print $5}' | cut -d'%' -f1)
+home_usage=$(df /home | tail -1 | awk '{print $5}' | cut -d'%' -f1)
 
-if [ $home_usage -gt 80 ]; then
+if [ "$home_usage" -gt 80 ]; then
     echo "WARNING: /home usage is ${home_usage}%" | wall
 fi
